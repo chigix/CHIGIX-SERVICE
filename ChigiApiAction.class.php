5c275570ac46c80f310fb2084e1cd3c582709b97
Richard Lea <chigix@zoho.com>
2013-02-25 17:20:15 +0800

    在NetBeans下微调了一下代码格式

--- ChigiApiAction.class.php
+++ ChigiApiAction.class.php
@@ -1,22 +1,27 @@
 <?php
+
 /**
  * 千木架构Api类定义抽象
  *
  * ★所有类不得自定义构造方法，但必须全部有定义的_ChigiApiInit()方法，否则报错。
  */
-abstract class ChigiApiAction extends Action  {
-	static public $appHost;
-	/**
-	 * 千木架构Api类定义初始化
-	 *
-	 * 负责安全访问，避免异步HTTP访问与攻击
-	 * 无正确POST令牌信息均返回404错误。
-	 */
-	public function _initialize(){
-		if (self::$appHost === null) {
-			_404();
-		}
-		$this->_ChigiApiInit();
-	}
+abstract class ChigiApiAction extends Action {
+
+    static public $appHost;
+
+    /**
+     * 千木架构Api类定义初始化
+     *
+     * 负责安全访问，避免异步HTTP访问与攻击
+     * 无正确POST令牌信息均返回404错误。
+     */
+    public function _initialize() {
+        if (self::$appHost === null) {
+            _404();
+        }
+        $this->_ChigiApiInit();
+    }
+
 }
+
 ?>
