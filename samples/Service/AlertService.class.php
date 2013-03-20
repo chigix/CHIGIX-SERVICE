c12910b4ac005bd1481fd7b4e4388c514f397100
Richard Lea <chigix@zoho.com>
2013-03-20 16:41:51 +0800

    AlertService增加链式书写支持

--- samples/Service/AlertService.class.php
+++ samples/Service/AlertService.class.php
@@ -13,9 +13,9 @@ class AlertService {
     public $message = "";
 
     /**
-     * 推送一个alert
+     * 推送一个alert，根据ReturnService实例
      * @param mixed $param 仅接受ReturnService的实例、统一返回规范的数组、符合操作码规范的三位数整型
-     * @return void
+     * @return \AlertService
      */
     public function push($param) {
         if (is_array($param)) {
@@ -26,7 +26,7 @@ class AlertService {
                 } else {
                     $this->message = $this->messageSuccess;
                 }
-                return;
+                return $this;
             } else {
                 $this->option = "alert-error";
                 if ($this->messageError === null) {
@@ -34,7 +34,7 @@ class AlertService {
                 } else {
                     $this->message = $this->messageError;
                 }
-                return;
+                return $this;
             }
         } elseif (is_object($param)) {
             if (($param->code >= 200) && ($param->code < 300)) {
@@ -48,7 +48,7 @@ class AlertService {
                         $this->message = $this->messageSuccess;
                     }
                 }
-                return;
+                return $this;
             } else {
                 $this->option = "alert-error";
                 if (get_class($param) == "ReturnService") {
@@ -60,28 +60,41 @@ class AlertService {
                         $this->message = $this->messageError;
                     }
                 }
-                return;
+                return $this;
             }
         } elseif (is_int($param)) {
             if (($param >= 200) && ($param < 300)) {
                 $this->option = "alert-success";
                 $this->message = $this->messageSuccess;
-                return;
+                return $this;
             } else {
                 $this->option = "alert-error";
                 $this->message = $this->messageError;
-                return;
+                return $this;
             }
         } else {
-            return;
+            return $this;
         }
     }
 
     /**
+     * 推送一个Alert，手动设置Alert参数
+     *
+     * @param string $message
+     * @param string $option 默认"alert-error"
+     */
+    public function pushSet($message , $option = 'alert-error') {
+        $this->message = $message;
+        $this->option = $option;
+        return $this;
+    }
+    /**
      * 启动alert前端推送
+     *
+     * @return \AlertService
      */
     public function alert() {
-        if ($this->message == "") {
+        if (empty($this->message)) {
             return;
         }
         ching("chijiAlertOn", true);
