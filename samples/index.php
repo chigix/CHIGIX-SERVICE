7711b617e2e1a9a829ff335059de7d6a920515fe
Richard Lea <chigix@zoho.com>
2013-04-06 13:03:16 +0800

    添加入口文件模板示例

---
+++ samples/index.php
@@ -0,0 +1,10 @@
+<?php
+//项目入口
+define('APP_NAME','Sugar');
+define('APP_PATH','./');
+
+
+define('APP_DEBUG',true);//您可以注释掉这行，关闭调试模式
+define('THINK_PATH','../ThinkPHP/');//定义正确的框架文件路径
+require_once THINK_PATH.'ThinkPHP.php';
+?>
