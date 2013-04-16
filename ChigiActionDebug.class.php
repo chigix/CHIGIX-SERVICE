f03ee59c6f4c64cb6ce880b1a6e56182f8106639
Richard Lea <chigix@zoho.com>
2013-04-17 02:30:52 +0800

    全新整合入千路前端算法作为框架前端渲染增强
    版本更新为1.6.8
    
    Signed-off-by: Richard Lea <chigix@zoho.com>

--- ChigiActionDebug.class.php
+++ ChigiActionDebug.class.php
@@ -98,7 +98,7 @@ abstract class ChigiAction extends Action {
             _404();
         }
         unset($_POST[C("TOKEN_NAME")]);
-        if ( isset($_SESSION['verify'])) {
+        if (isset($_SESSION['verify'])) {
             if ($_SESSION['verify'] != md5($_POST['verify'])) {
                 $this->error("验证码错误");
             }
@@ -198,7 +198,153 @@ abstract class ChigiAction extends Action {
     }
 
     protected function display($templateFile = '', $charset = '', $contentType = '', $content = '', $prefix = '') {
-        parent::display($templateFile, $charset, $contentType, $content, $prefix);
+        if (APP_DEBUG) {
+            if ($templateFile == '') {
+                $templateFile = ACTION_NAME;
+            }
+            $this->tempCheck($templateFile); //检测模板文件是否存在，若不存在则自动生成
+            // 设置MODULE_LIST
+            C('CHIJI.MODULE_LIST', array());
+            // <editor-fold defaultstate="collapsed" desc="初始化视图类，摘自Action类initView方法">
+            //实例化视图类
+            if (!$this->view)
+                $this->view = Think::instance('View');
+            // 模板变量传值
+            if ($this->tVar)
+                $this->view->assign($this->tVar);
+            // </editor-fold>
+            // <editor-fold defaultstate="collapsed" desc="摘自View类display方法">
+            G('viewStartTime');
+            tag('view_begin', $templateFile);
+            $output = $this->view->fetch($templateFile, $content, $prefix);
+            // </editor-fold>
+            //开始模块前端动态编译
+            $lessFile = ""; //用于存放生成的Less模块导入文件列表
+            if (CHIGITEMPLATE_OK !== true) {
+                //本标识位于chigiTemplate.class.php构造方法中声明
+                throw_exception('模板引擎加载错误，别名配置“' . APP_NAME . '/Conf/alias.php”中应定义“ThinkTemplate”配置项！！');
+            }
+            // <editor-fold defaultstate="collapsed" desc="Trace模板Module加载列表">
+
+            trace("【模板Module加载列表】#############");
+            foreach (C('CHIJI.MODULE_LIST') as $key => $value) {
+                trace('→' . $key . "：" . $value);
+            }
+            trace("[模板Module加载列表]================");
+            // </editor-fold>
+            //★Less编译
+            // <editor-fold defaultstate="collapsed" desc="Less编译">
+            import('ORG.Chiji.Lessc');
+            $less = new lessc;
+            //##处理module编译顺序列表并生成LESS的导入文件列表(String)
+            foreach (C('CHIJI.MODULE_LIST') as $value) {
+                $class = str_replace(array(':', '#'), array('/', '.'), $value);
+                $class_strut = explode('/', $class);
+                $lessFileItem = $class_strut[1] . '.less';
+                $importDirItem = THEME_PATH . $class_strut[0] . '/';
+                $less->addImportDir($importDirItem);
+                if (file_exists($importDirItem . $lessFileItem)) {
+                    $lessFile .= '@import ' . $lessFileItem . ';' . PHP_EOL;
+                }
+            }
+            $less->importDir = array_unique($less->importDir); //合并重复项目
+            //##编译及写入page-CSS文件
+            if (C('CHIJI.LESS_COMPRESS')) {
+                $less->setFormatter(C('CHIJI.LESS_COMPRESS'));
+            } else {
+                $less->setFormatter("compressed");
+            }
+            if (file_put_contents('./../Chiji/' . APP_NAME . '/css/page-' . $templateFile . '.css', $less->compile($lessFile))) {
+                trace('Chiji/' . APP_NAME . '/css/page-' . $templateFile . '.css', "页面CSS渲染完毕");
+            } else {
+                if ($less->compile($lessFile) == '') {
+                    trace("页面CSS无内容");
+                } else {
+                    trace("页面CSS渲染失败");
+                }
+            }
+            // </editor-fold>
+            //★JavaScript模块编译
+            // <editor-fold defaultstate="collapsed" desc="JavaScript模块编译">
+            //##处理module编译顺序列表并生成JS合并
+            $jsCombinedString = "";
+            foreach (C('CHIJI.MODULE_LIST') as $value) {
+                $class = str_replace(array(':', '#'), array('/', '.'), $value);
+                $class_strut = explode('/', $class);
+                $jsFileItem = $class_strut[1];
+                $importDirItem = THEME_PATH . $class_strut[0] . '/';
+                if (file_exists($importDirItem . $jsFileItem . '.js')) {
+                    $jsCombinedString .= file_get_contents($importDirItem . $jsFileItem . '.js') . PHP_EOL;
+                }
+                if (C('CHIJI.JS_DEBUG') && file_exists($importDirItem . $jsFileItem . '-test.js')) {
+                    $jsCombinedString .= file_get_contents($importDirItem . $jsFileItem . '-test.js') . PHP_EOL;
+                }
+            }
+            //##JS代码压缩
+            if (!C("CHIJI.JS_DEBUG")) {
+                import('ORG.Chiji.JsCompress');
+                $jsCombinedString = chijiJsCompress($jsCombinedString);
+            }
+            switch (file_put_contents('./../Chiji/' . APP_NAME . '/js/page-' . $templateFile . '.js', $jsCombinedString)) {
+                case 0:
+                    trace("页面JS无内容");
+                    break;
+                case false:
+                    trace("页面JS渲染失败");
+                    break;
+                default:
+                    trace('Chiji/' . APP_NAME . '/css/page-' . $templateFile . '.js', "页面JS渲染完毕");
+                    break;
+            }
+            // </editor-fold>
+            //★输出前端页面HTML代码至浏览器
+            // <editor-fold defaultstate="collapsed" desc="摘自View类render方法，请视当前版本的render方法进行改动">
+            $charset = C('DEFAULT_CHARSET');
+            $contentType = C('TMPL_CONTENT_TYPE');
+            // 网页字符编码
+            header('Content-Type:' . $contentType . '; charset=' . $charset);
+            header('Cache-control: ' . C('HTTP_CACHE_CONTROL'));  // 页面缓存控制
+            header('X-Powered-By:ThinkPHP');
+            echo $output;
+            // </editor-fold>
+            tag('view_end');
+        }
+    }
+
+    protected function tempCheck($pageName) {
+        $pagePath = THEME_PATH . 'Index/' . $pageName . '.html';
+        $dirPath = THEME_PATH . ucfirst($pageName);
+        $pageData = file_get_contents(CHIGI_PATH . 'html/index.html');
+        $pageData = $this->keywordReplace($pageData, $pageName);
+        $starterData = file_get_contents(CHIGI_PATH . 'html/StarterMODULE.html');
+        $starterData = $this->keywordReplace($starterData, $pageName);
+        $enderData = file_get_contents(CHIGI_PATH . 'html/EnderMODULE.html');
+        $enderData = $this->keywordReplace($enderData, $pageName);
+        if (file_exists($pagePath)) {
+            return;
+        } else {
+            file_put_contents($pagePath, $pageData);
+            if (!mkdir($dirPath)) {
+                echo "创建目录失败";
+                dump($dirPath);
+                exit;
+            }
+            file_put_contents($dirPath . '/StarterMODULE.html', $starterData);
+            file_put_contents($dirPath . '/EnderMODULE.html', $enderData);
+            return;
+        }
+    }
+
+    protected function keywordReplace($data, $pageName) {
+        $replace = array(
+            '{PAGENAME}' => $pageName,
+            '{APPNAME}' => APP_NAME,
+            '{PACKAGENAME}' => ucfirst($pageName),
+        );
+        foreach ($replace as $key => $value) {
+            $data = str_replace($key, $value, $data);
+        }
+        return $data;
     }
 
     protected function fetch($templateFile = '', $content = '', $prefix = '') {
--- ChigiData.class.php
+++ ChigiData.class.php
@@ -7,14 +7,28 @@
  */
 class ChigiData {
 
-    private $__data;
+    public $__data;
     private $__view = 'table';
     private $__ext = 'Strap';
-    private $__output = "";
-    public function __construct($dataSource,$dataExt = 'Strap') {
-        $this->__data = $dataSource;
-        $this->__ext = $dataExt;
-        import(CHIGI_PATH . 'DataExt/' . $dataExt . 'View');
+    private $__output = "BANKAI";
+
+    public function __construct($dataSource, $dataExt = 'Strap') {
+        if (is_object($dataSource) && get_class($dataSource) == "ChigiReturn") {
+            //按千木返回标准对象处理
+            $this->__data = $dataSource;
+            $this->__ext = $dataExt;
+        } elseif (is_array($dataSource)) {
+            if (array_key_exists("status", $dataSource) && array_key_exists("info", $dataSource)) {
+                //按千木返回标准数组处理
+                $this->__data = isset($dataSource['data']) ? $dataSource['data'] : null;
+            } else {
+                //按普通数据数组处理
+                $this->__data = $dataSource;
+            }
+        } else {
+            //非数组非对象的处理方式
+            $this->__data = $dataSource;
+        }
     }
 
     /**
@@ -25,6 +39,7 @@ class ChigiData {
     public function get($dataSource) {
         $this->__data = $dataSource;
     }
+
     /**
      * 设定并返回数据视图内容
      *
@@ -32,15 +47,56 @@ class ChigiData {
      * @param String $dataExt 临时改变目标渲染驱动类
      * @return string 数据视图层渲染结果
      */
-    public function view($type , $dataExt = null) {
-        $this->__view = $type;
-        $renderResult = "";
-        $this->__output = $renderResult;
-        return $renderResult;
+    public function view($type, $name, $isLock = false, $pageName = null) {
+        if (APP_DEBUG && !$isLock) {
+            //编译模式
+            $this->__view = $type;
+            if ($pageName === null) {
+                $pageName = ACTION_NAME;
+            }
+
+            // 数据格式转换
+            $cols = array_keys($this->__data[0]);
+            $cols = array_merge(array("__key"), $cols);
+
+            // 数据可视化渲染
+            $result = qp('<table id="' . ucfirst($name) . 'MODULE"><thead><tr></tr></thead><tbody></tbody></table>', '#' . ucfirst($name) . 'MODULE');
+            $table = $result->find('table');
+            $table->addClass('table');
+            $table->addClass('table-bordered');
+            $table->addClass('table-hover');
+            $table->addClass('table-striped');
+            foreach ($cols as $value) {
+                $result->find('thead tr')->append("<th>$value</th>");
+            }
+            $tbody = $table->find('tbody');
+            $tbody->append('<volist name="' . $name . '" id="vo" key="__key"></volist>');
+            $volist = $tbody->find('volist');
+            $newLine = qp('<tr></tr>', 'tr');
+            foreach ($cols as $value) {
+                if ($value == "__key") {
+                    $newLine->append('<td>{$__key}</td>');
+                } else {
+                    $newLine->append('<td>{$vo["' . $value . '"]}</td>');
+                }
+            }
+            $newLine->appendTo($volist);
+
+            // 输出渲染结果
+            if (file_put_contents(THEME_PATH . "/$pageName/" . ucfirst($name) . "MODULE.html", $result->html())) {
+                trace(THEME_PATH . "/$pageName/" . $name . "MODULE.html", $name . "MODULE模板文件渲染完毕");
+            } else {
+                trace("页面CSS渲染失败");
+            }
+        }
+        //$this->__output = $result->html();
+        return $this->__data;
     }
+
     public function __toString() {
         return $this->__output;
     }
+
 }
 
 ?>
