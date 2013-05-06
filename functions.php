f51f0ff1b1c4b060c60e7fb5abd533eceab96a96
Richard Lea <chigix@zoho.com>
2013-05-06 21:27:56 +0800

    修复入口在URL中的不输出BUG

--- functions.php
+++ functions.php
@@ -341,7 +341,7 @@ function redirect_link($addr, $params = array(), $domain = null,$sidShow = true)
     if (substr($addr, -5) == 'index') {
         if (substr($addr, 0, 5) == 'Index') {
             //定位为Index/index，即总域名（注：千木架构规范，Index控制器下仅能存在一个操作）
-            $addr = is_null($domain)?((is_ssl() ? 'https://' : 'http://') . $_SERVER['HTTP_HOST']):$domain;
+            $addr = is_null($domain)?((is_ssl() ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] . __APP__):($domain . __APP__);
             $addr .= '/';
         }  else {
             //定位地址中有可省略的index，故结尾不留.html
