f44b187f2f2f101996abea868ace498f87f01e4c
Richard Lea <chigix@zoho.com>
2013-05-06 21:27:06 +0800

    加入USER_AGENT检测机制 优化COOKIE检测算法
    Signed-off-by: Richard Lea <chigix@zoho.com>

--- Ching.class.php
+++ Ching.class.php
@@ -10,6 +10,7 @@ class CHING {
 
     private static $__instance;
     public static $CID;
+    public static $BOT = '';
 
     /**
      * 当前客户端中是否有cookie("sid")
@@ -35,7 +36,7 @@ class CHING {
                 self::$__instance = new CHING($cid, $expire);
                 //继续现已有会话的内容
                 self::$__instance->setAll($tmp);
-            }  else {
+            } else {
                 //开启全新的会话内容
                 self::$__instance = new CHING($cid, $expire);
             }
@@ -76,15 +77,150 @@ class CHING {
         }
         $this->__data = $content;
         // </editor-fold>
-        // <editor-fold defaultstate="collapsed" desc="侦测客户端浏览器是否支持COOKIE">
-        if ((!self::$COOKIE_STATUS) && (!isset($_REQUEST['sid']))) {
+        // <editor-fold defaultstate="collapsed" desc="判断是否来自搜索引擎">
+        if (empty(self::$BOT)) {
+            //说明当前是初次启动CHING会话
+            $user_agent = strtolower($_SERVER['HTTP_USER_AGENT']);
+            if (strpos($user_agent, 'baiduspider') !== false) {
+                self::$BOT = 'C_BAIDU';
+                return;
+            } elseif (strpos($user_agent, 'googlebot') !== false) {
+                self::$BOT = 'C_GOOGLE';
+                return;
+            } elseif (strpos($user_agent, 'msnbot') !== false) {
+                self::$BOT = 'C_MSN';
+                return;
+            } elseif (strpos($user_agent, 'slurp') !== false) {
+                self::$BOT = 'C_YAHOO';
+                return;
+            } elseif (strpos($user_agent, 'sohu-search') !== false) {
+                self::$BOT = 'C_SOHU';
+                return;
+            } elseif (strpos($user_agent, 'lycos') !== false) {
+                self::$BOT = 'C_LYCOS';
+                return;
+            } elseif (strpos($user_agent, 'robozilla') !== false) {
+                self::$BOT = 'C_ROBOZILLA';
+                return;
+            } elseif (strpos($user_agent, 'tencenttraveler') !== false) {
+                self::$BOT = 'C_TTBROWSER';
+                return;
+            } elseif (strpos($user_agent, 'baidugame') !== false) {
+                self::$BOT = 'C_BAIDUGAME';
+                return;
+            } elseif (strpos($user_agent, 'sosospider+') !== false) {
+                self::$BOT = 'C_SOSO';
+                return;
+            } elseif (strpos($user_agent, 'sogou web spider') !== false) {
+                self::$BOT = 'C_SOGOU';
+                return;
+            } elseif (strpos($user_agent, 'ia_archiver') !== false) {
+                self::$BOT = 'C_ALEXA';
+                return;
+            } elseif (strpos($user_agent, 'youdaobot') !== false) {
+                self::$BOT = 'C_YOUDAO';
+                return;
+            } elseif (strpos($user_agent, 'voila') !== false) {
+                self::$BOT = 'C_VOILA';
+                return;
+            } elseif (strpos($user_agent, 'yandex bot') !== false) {
+                self::$BOT = 'C_YANDEX';
+                return;
+            } elseif (strpos($user_agent, 'bspider') !== false) {
+                self::$BOT = 'C_JP-BSPIDER';
+                return;
+            } elseif (strpos($user_agent, 'twiceler') !== false) {
+                self::$BOT = 'C_TWICELER';
+                return;
+            } elseif (strpos($user_agent, 'sogou spider') !== false) {
+                self::$BOT = 'C_SOGOU';
+                return;
+            } elseif (strpos($user_agent, 'speedy spider') !== false) {
+                self::$BOT = 'C_ENTEIREWEB.com';
+                return;
+            } elseif (strpos($user_agent, 'google adsense') !== false) {
+                self::$BOT = 'C_GOOGLE-AD';
+                return;
+            } elseif (strpos($user_agent, 'heritrix') !== false) {
+                self::$BOT = 'C_HERITRIX';
+                return;
+            } elseif (strpos($user_agent, 'python-urllib') !== false) {
+                self::$BOT = 'C_PYTHON-URLLIB';
+                return;
+            } elseif (strpos($user_agent, 'alexa (ia archiver)') !== false) {
+                self::$BOT = 'C_ALEXA';
+                return;
+            } elseif (strpos($user_agent, 'ask') !== false) {
+                self::$BOT = 'C_ASK';
+                return;
+            } elseif (strpos($user_agent, 'exabot') !== false) {
+                self::$BOT = 'C_EXALEAD';
+                return;
+            } elseif (strpos($user_agent, 'custo') !== false) {
+                self::$BOT = 'C_CUSTO';
+                return;
+            } elseif (strpos($user_agent, 'outfoxbot/yodaobot') !== false) {
+                self::$BOT = 'C_YOUDAO';
+                return;
+            } elseif (strpos($user_agent, 'yacy') !== false) {
+                self::$BOT = 'C_YACY-PEER';
+                return;
+            } elseif (strpos($user_agent, 'surveybot') !== false) {
+                self::$BOT = 'C_MYSPACE';
+                return;
+            } elseif (strpos($user_agent, 'legs') !== false) {
+                self::$BOT = 'C_80LEGS';
+                return;
+            } elseif (strpos($user_agent, 'nutch') !== false) {
+                self::$BOT = 'C_NUTCH';
+                return;
+            } elseif (strpos($user_agent, 'the web archive (ia archiver)') !== false) {
+                self::$BOT = 'C_ALEXA';
+                return;
+            } elseif (strpos($user_agent, 'perl tool') !== false) {
+                self::$BOT = 'C_PERL';
+                return;
+            } elseif (strpos($user_agent, 'bingbot') !== false) {
+                self::$BOT = 'C_BING';
+                return;
+            } elseif (strpos($user_agent, 'yrspider') !== false) {
+                self::$BOT = 'C_YUNRANG';
+                return;
+            } elseif (strpos($user_agent, 'jikespider') !== false) {
+                self::$BOT = 'C_JIKE';
+                return;
+            } elseif (strpos($user_agent, 'trident') !== false) {
+                self::$BOT = 'B_IE';
+            } elseif (strpos($user_agent, 'msie') !== false) {
+                self::$BOT = 'B_IE';
+            } elseif (strpos($user_agent, 'webkit') !== false) {
+                self::$BOT = 'B_WEBKIT';
+            } elseif (strpos($user_agent, 'presto') !== false) {
+                self::$BOT = 'B_PRESTO';
+            } elseif (strpos($user_agent, 'gecko') !== false) {
+                self::$BOT = 'B_GECKO';
+            } else {
+                _404();
+            }
+        }
+        // </editor-fold>
+        // <editor-fold defaultstate="collapsed" desc="↓侦测客户端浏览器是否支持COOKIE">
+        if (substr(self::$BOT, 0, 2) == 'C_') {
+            self::$COOKIE_STATUS = true;
+        }
+        if ((!self::$COOKIE_STATUS) && (!isset($_REQUEST['sid'])) && (substr(self::$BOT, 0, 2) == 'B_')) {
             //第一次访问本网站，或浏览器不支持COOKIE
             //即浏览器端没有COOKIE信息
-            Dispatcher::dispatch();
             if (file_exists((LIB_PATH . '/Action/EmptyAction.class.php'))) {
+                Dispatcher::dispatch();
                 //已定义 空模块，则跳转检测客户端是否支持COOKIE
                 redirectHeader('/On/', array('type' => 'checkcookie', 'sid' => self::$CID, 'iframe' => (is_ssl() ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']), null);
+            } elseif (APP_DEBUG === TRUE) {
+                trace('[0]未定义空模块，无法进行客户端COOKIE支持检测', '', 'NOTIC');
             }
+        }
+        if (APP_DEBUG && self::$COOKIE_STATUS && isset($_REQUEST['sid'])) {
+            trace('[0]未定义空模块，无法进行客户端COOKIE支持检测', '', 'NOTIC');
         }
         // </editor-fold>
     }
@@ -130,6 +266,7 @@ class CHING {
         $this->__data = $data;
         return;
     }
+
     /**
      * 删除指定的CHING 值
      *
